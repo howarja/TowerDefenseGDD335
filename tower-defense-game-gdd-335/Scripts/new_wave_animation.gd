258d8a93ff1fd9294b0da776c98a909e93e5c7d1
James <howarja@whs.school.nz>
2026-08-14 12:51:33 +1200

    added end wave panel

--- tower-defense-game-gdd-335/Scripts/new_wave_animation.gd
+++ tower-defense-game-gdd-335/Scripts/new_wave_animation.gd
@@ -1,8 +1,14 @@
 extends AnimationPlayer
 
 func _ready() -> void:
-	Globals.enemyManager.waveBegin.connect(playAnim);
-	$newWavePanel/Text.text = "[center]Wave: " + str(Globals.enemyManager.getCurrentWave()+1);
+	Globals.enemyManager.waveBegin.connect(startAnim);
+	Globals.enemyManager.waveComplete.connect(endAnim);
 	
-func playAnim():
+	
+func startAnim():
+	$newWavePanel/Text.text = "[center]Wave: " + str(Globals.enemyManager.getCurrentWave()+1);
 	play("NewWave")
+
+func endAnim():
+	$newWavePanel/Text.text = "[center]Wave won!";
+	play("waveComplete")
